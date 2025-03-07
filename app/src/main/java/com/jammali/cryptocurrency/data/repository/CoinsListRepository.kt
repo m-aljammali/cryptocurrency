486426619817d6a8e95c760b997a080da7c84fb7
mohammed aljammali <mohammad.aljammali@gmail.com>
2025-03-07 16:27:28 +1100

    show toast if api call failed

--- app/src/main/java/com/jammali/cryptocurrency/data/repository/CoinsListRepository.kt
+++ app/src/main/java/com/jammali/cryptocurrency/data/repository/CoinsListRepository.kt
@@ -2,12 +2,14 @@ package com.jammali.cryptocurrency.data.repository
 
 import android.content.Context
 import android.util.Log
-import com.jammali.cryptocurrency.data.local.database.CoinsListEntity
-import com.jammali.cryptocurrency.utils.Constants
+import android.view.Gravity
+import android.widget.Toast
 import com.jammali.cryptocurrency.data.api.Result
 import com.jammali.cryptocurrency.data.api.succeeded
+import com.jammali.cryptocurrency.data.local.database.CoinsListEntity
 import com.jammali.cryptocurrency.data.storePreference
 import com.jammali.cryptocurrency.utils.CacheUtils
+import com.jammali.cryptocurrency.utils.Constants
 import kotlinx.coroutines.flow.Flow
 
 
@@ -18,8 +20,9 @@ class CoinsListRepository (
 
 
     suspend fun coinsList(targetCur: String , marketCapDesc: String, perPage: String, page: String, context: Context) {
-
-        Log.d("CoinsListRepository", "coinsList")
+        val toast =
+            Toast.makeText(context, Constants.GENERIC_ERROR, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0)
         when (val result = coinsListRemoteDataSource.coinsList(targetCur, marketCapDesc , perPage, page )) {
             is Result.Success -> {
                 if (result.succeeded) {
@@ -46,10 +49,17 @@ class CoinsListRepository (
 
                     Result.Success(true)
                 } else {
+
+                    toast.show()
+                    Log.d("CoinsListRepository", "error")
                     Result.Error(Constants.GENERIC_ERROR)
                 }
             }
-            else -> result as Result.Error
+            else -> {
+
+                toast.show()
+
+                result as Result.Error}
         }
     }
 
--- app/src/main/java/com/jammali/cryptocurrency/utils/Constants.kt
+++ app/src/main/java/com/jammali/cryptocurrency/utils/Constants.kt
@@ -2,5 +2,5 @@ package com.jammali.cryptocurrency.utils
 
 object Constants {
     const val BASE_URL = "https://api.coingecko.com/api/v3/"
-    const val GENERIC_ERROR = "Something went wrong, try again!"
+    const val GENERIC_ERROR = "Something went wrong, check internet connection then try again!"
 }
